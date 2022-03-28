bc8cdf36ca80017ca1e809857b66102ad55844ab
TanNguyen <phuongtan12357nguyen@gmail.com>
2022-03-28 23:33:32 +0700

    update render view apply share partial

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -18,14 +18,26 @@ class Controller
      * @param $viewPath
      * @param array $data
      */
-    protected function renderView($viewPath, array $data = [])
+    public function renderView($viewPath, array $data = [])
+    {
+        $this->renderPartial('frontend.partial.header');
+        $this->renderPartial($viewPath, $data);
+        $this->renderPartial('frontend.partial.footer');
+    }
+
+    /**
+     * Render partial file
+     * @param $partialPath
+     * @param array $data
+     */
+    public function renderPartial($partialPath, array $data = [])
     {
         //get data
         foreach ($data as $key => $value) {
             $$key = $value;
         }
 
-        $viewPathFile = self::VIEW_PATH . '/' . str_replace('.', '/', $viewPath) . '.php';
+        $viewPathFile = self::VIEW_PATH . '/' . str_replace('.', '/', $partialPath) . '.php';
         require $viewPathFile;
     }
 
