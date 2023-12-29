d40858c38684f133078328cc61af405d453c35ae
Tan Nguyen <tannp27@gmail.com>
2023-12-29 14:11:45 +0700

    update readme and model in core

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -66,7 +66,7 @@ class Model extends Database
     public static function slug($str): array|string
     {
         $str = (new Model)->convert_name($str);
-        $str = strtolower($str); //mb_strtolower($str, 'UTF-8');
+        $str = strtolower($str);
         return str_replace(' ', '-', $str);
     }
 
@@ -79,19 +79,19 @@ class Model extends Database
      */
     public function convert_name($str): array|string|null
     {
-        $str = preg_replace("/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/", 'a', $str);
-        $str = preg_replace("/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/", 'e', $str);
-        $str = preg_replace("/(ì|í|ị|ỉ|ĩ)/", 'i', $str);
-        $str = preg_replace("/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/", 'o', $str);
-        $str = preg_replace("/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/", 'u', $str);
-        $str = preg_replace("/(ỳ|ý|ỵ|ỷ|ỹ)/", 'y', $str);
+        $str = preg_replace("/([àáạảãâầấậẩẫăằắặẳẵ])/", 'a', $str);
+        $str = preg_replace("/([èéẹẻẽêềếệểễ])/", 'e', $str);
+        $str = preg_replace("/([ìíịỉĩ])/", 'i', $str);
+        $str = preg_replace("/([òóọỏõôồốộổỗơờớợởỡ])/", 'o', $str);
+        $str = preg_replace("/([ùúụủũưừứựửữ])/", 'u', $str);
+        $str = preg_replace("/([ỳýỵỷỹ])/", 'y', $str);
         $str = preg_replace("/(đ)/", 'd', $str);
-        $str = preg_replace("/(À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/", 'A', $str);
-        $str = preg_replace("/(È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/", 'E', $str);
-        $str = preg_replace("/(Ì|Í|Ị|Ỉ|Ĩ)/", 'I', $str);
-        $str = preg_replace("/(Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/", 'O', $str);
-        $str = preg_replace("/(Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/", 'U', $str);
-        $str = preg_replace("/(Ỳ|Ý|Ỵ|Ỷ|Ỹ)/", 'Y', $str);
+        $str = preg_replace("/([ÀÁẠẢÃÂẦẤẬẨẪĂẰẮẶẲẴ])/", 'A', $str);
+        $str = preg_replace("/([ÈÉẸẺẼÊỀẾỆỂỄ])/", 'E', $str);
+        $str = preg_replace("/([ÌÍỊỈĨ])/", 'I', $str);
+        $str = preg_replace("/([ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ])/", 'O', $str);
+        $str = preg_replace("/([ÙÚỤỦŨƯỪỨỰỬỮ])/", 'U', $str);
+        $str = preg_replace("/([ỲÝỴỶỸ])/", 'Y', $str);
         $str = preg_replace("/(Đ)/", 'D', $str);
         $str = preg_replace("/(\“|\”|\‘|\’|\,|\!|\&|\;|\@|\#|\%|\~|\`|\=|\_|\'|\]|\[|\}|\{|\)|\(|\+|\^)/", '-', $str);
         return preg_replace("/( )/", '-', $str);
